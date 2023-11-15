9cdb624d0565409f2a4c73c9458c7985d88ffbcc
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-15 16:47:25 +0100

    LMGateway needs permissions to fetch AMI ID from SSM

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -196,3 +196,22 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.lmgateway.id
   policy_arn = data.aws_iam_policy.ssm.arn
 }
+
+data "aws_iam_policy_document" "lmgateway" {
+  statement {
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+    ]
+
+    resources = [
+      aws_ssm_parameter.ami_x86_64.arn,
+      aws_ssm_parameter.ami_arm64.arn
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lmgateway" {
+  role   = aws_iam_role.lmgateway.name
+  policy = data.aws_iam_policy_document.lmgateway.json
+}
