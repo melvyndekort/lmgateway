ce952dd5e882f5541468358a0b320d93d903b048
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-15 16:55:32 +0100

    Workaround for Auto Scaling group limitation

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -6,80 +6,91 @@ resource "aws_autoscaling_group" "lmgateway" {
   min_size            = 0
   capacity_rebalance  = true
 
-  mixed_instances_policy {
-    instances_distribution {
-      on_demand_percentage_above_base_capacity = 0
-    }
-
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.arm.id
-        version            = "$Latest"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.arm.id
-          version            = "$Latest"
-        }
-        instance_type = "t4g.nano"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t3a.nano"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t3.nano"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t2.nano"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t1.micro"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.arm.id
-          version            = "$Latest"
-        }
-        instance_type = "t4g.micro"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t3a.micro"
-      }
-
-      override {
-        launch_template_specification {
-          launch_template_id = aws_launch_template.x86.id
-          version            = "$Latest"
-        }
-        instance_type = "t3.micro"
-      }
-    }
+  launch_template {
+    id      = aws_launch_template.arm.id
+    version = "$Latest"
   }
+
+# Currently, you cannot use a launch template that specifies a Systems Manager parameter
+# instead of an AMI ID on an Auto Scaling group with a mixed instances policy.
+#
+# https://docs.aws.amazon.com/autoscaling/ec2/userguide/using-systems-manager-parameters.html#using-systems-manager-parameters-limitations
+#
+#
+#  mixed_instances_policy {
+#    instances_distribution {
+#      on_demand_percentage_above_base_capacity = 0
+#    }
+#
+#    launch_template {
+#      launch_template_specification {
+#        launch_template_id = aws_launch_template.arm.id
+#        version            = "$Latest"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.arm.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t4g.nano"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t3a.nano"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t3.nano"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t2.nano"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t1.micro"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.arm.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t4g.micro"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t3a.micro"
+#      }
+#
+#      override {
+#        launch_template_specification {
+#          launch_template_id = aws_launch_template.x86.id
+#          version            = "$Latest"
+#        }
+#        instance_type = "t3.micro"
+#      }
+#    }
+#  }
 }
