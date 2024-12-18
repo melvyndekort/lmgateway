0d9bab44ca02dcadf5287dd4422990f65989663c
Melvyn de Kort <melvyn@mdekort.nl>
2024-12-19 00:15:59 +0100

    Enable IPv4 stuff, because we use nat64.net now

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# TODO: SSM cannot be reached over IPv6 yet, exiting
-exit 0
-
-
 # Retrieve New Relic license key from SSM Parameter Store
 VALUE="$(aws ssm get-parameters --names /mdekort/lmgateway/newrelic_key --with-decryption --query 'Parameters[0].Value' --output text)"
 
